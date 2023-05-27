a9d950a86b1b1464d8cdb6641d080eb3593a1c8a
Gustavo Lucas <gustavo.lukas04@gmail.com>
2023-05-27 15:52:09 -0300

    Create Paths.py

---
+++ Paths.py
@@ -0,0 +1,31 @@
+paths = {
+    # DAUNTLESS
+    "dauntless": "C:\Program Files\Epic Games\Dauntless\Dauntless.exe",
+    "downloads": "C:\Program Files\Epic Games\Dauntless\Dauntless.exe",
+    "dá 13": "C:\Program Files\Epic Games\Dauntless\Dauntless.exe",
+    "dá um 13": "C:\Program Files\Epic Games\Dauntless\Dauntless.exe",
+    "não três": "C:\Program Files\Epic Games\Dauntless\Dauntless.exe",
+
+    # LEAGUE OF LEGENDS
+    "lol": "C:\Riot Games\Riot Client\RiotClientServices.exe",
+    "uol": "C:\Riot Games\Riot Client\RiotClientServices.exe",
+    "league of legends": "C:\Riot Games\Riot Client\RiotClientServices.exe",
+    "liga das lendas": "C:\Riot Games\Riot Client\RiotClientServices.exe",
+
+    # STEAM
+    "steam": "C:\Program Files (x86)\Steam\steam.exe",
+    "tim": "C:\Program Files (x86)\Steam\steam.exe",
+
+    # EPIC
+    "epic": "C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe",
+    "epic games": "C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe",
+    "epic games launcher": "C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe",
+
+    # FIREFOX
+    "firefox": "C:\Program Files\Mozilla Firefox\firefox.exe",
+
+    # CHROME
+    "google": "C:\Program Files\Google\Chrome\Application\chrome.exe",
+    "chrome": "C:\Program Files\Google\Chrome\Application\chrome.exe",
+    "google chrome": "C:\Program Files\Google\Chrome\Application\chrome.exe",
+}
